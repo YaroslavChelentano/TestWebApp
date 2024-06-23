851f250e1a13f8bb88fa05794d3bcaf08577277a
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2024-06-24 00:49:44 +0300

    add project

---
+++ TestAppLecture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestAppLecture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
